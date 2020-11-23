b698713f671cc539d77afe6c49c13de98e862d70
Axel Huebl <axel.huebl@plasma.ninja>
2020-11-22 18:51:18 -0800

    Add: 2D & RZ
    Also build 2D and RZ binaries.

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,30 +14,33 @@ elif [[ ${CXXFLAGS} == *"-std="* ]]; then
     exit 1
 fi
 
-cmake \
-    -S . -B build                         \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo     \
-    -DCMAKE_VERBOSE_MAKEFILE=ON           \
-    -DCMAKE_CXX_STANDARD=${CXX_STANDARD}  \
-    -DCMAKE_INSTALL_LIBDIR=lib            \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}      \
-    -DWarpX_amrex_branch=${PKG_VERSION}   \
-    -DWarpX_openpmd_internal=OFF          \
-    -DWarpX_picsar_branch=d60c72ff5aa15dbd7e225654964b6c4fb10d52e2 \
-    -DWarpX_ASCENT=OFF  \
-    -DWarpX_OPENPMD=ON  \
-    -DWarpX_PSATD=OFF   \
-    -DWarpX_QED=ON      \
-    -DWarpX_DIMS=3      \
-    ${SRC_DIR}
+for dim in "2" "3" "RZ"
+do
+    cmake \
+        -S . -B build                         \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo     \
+        -DCMAKE_VERBOSE_MAKEFILE=ON           \
+        -DCMAKE_CXX_STANDARD=${CXX_STANDARD}  \
+        -DCMAKE_INSTALL_LIBDIR=lib            \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX}      \
+        -DWarpX_amrex_branch=${PKG_VERSION}   \
+        -DWarpX_openpmd_internal=OFF          \
+        -DWarpX_picsar_branch=d60c72ff5aa15dbd7e225654964b6c4fb10d52e2 \
+        -DWarpX_ASCENT=OFF  \
+        -DWarpX_OPENPMD=ON  \
+        -DWarpX_PSATD=OFF   \
+        -DWarpX_QED=ON      \
+        -DWarpX_DIMS=${dim} \
+        ${SRC_DIR}
 
-cmake --build build --parallel ${CPU_COUNT}
+    cmake --build build --parallel ${CPU_COUNT}
 
-# future:
-#CTEST_OUTPUT_ON_FAILURE=1 make ${VERBOSE_CM} test
+    # future:
+    #CTEST_OUTPUT_ON_FAILURE=1 make ${VERBOSE_CM} test
+done
 
 # future:
 #make install
 mkdir -p ${PREFIX}/bin
-cp build/bin/warpx.3d.MPI.OMP.DP.OPMD.QED ${PREFIX}/bin/
+cp build/bin/warpx.* ${PREFIX}/bin/
 
