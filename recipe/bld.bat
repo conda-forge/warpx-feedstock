854c2f6c47dcbe53c6c0834d58441b4eee6b85d6
Isuru Fernando <isuruf@gmail.com>
2021-03-02 21:09:27 -0600

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,11 +9,6 @@ set "RANLIB=llvm-ranlib.exe"
 set "NM=llvm-nm.exe"
 set "LD=lld-link.exe"
 
-set "CPPFLAGS_USED=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld -fno-aligned-allocation"
-set "LDFLAGS=-nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld -Wl,-defaultlib:%CONDA_PREFIX%/lib/clang/@PKG_VERSION@/lib/windows/clang_rt.builtins-x86_64.lib"
-set "CFLAGS=@CFLAGS@ %CPPFLAGS_USED%"
-set "CXXFLAGS=@CXXFLAGS@ %CPPFLAGS_USED%"
-
 :: simple install prep
 ::   copy all warpx*.exe and warpx*.dll files
 if not exist %LIBRARY_PREFIX%\bin md %LIBRARY_PREFIX%\bin
