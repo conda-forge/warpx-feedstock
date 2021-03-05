758ee5b4c2b0765d4985b6a8d214e436f74efbc0
Axel Huebl <axel.huebl@plasma.ninja>
2021-03-04 21:38:17 -0800

    Windows: no FFTW/PSATD
    ```
    [234/235] cmd.exe /C "cd . && %BUILD_PREFIX%\Library\bin\cmake.exe -E vs_link_exe --intdir=CMakeFiles\app.dir --rc=C:\PROGRA~2\WI3CF2~1\10\bin\100177~1.0\x64\rc.exe --mt=C:\PROGRA~2\WI3CF2~1\10\bin\100177~1.0\x64\mt.exe --manifests  -- %BUILD_PREFIX%\Library\bin\lld-link.exe  @CMakeFiles\app.rsp  /out:bin\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe /implib:lib\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.lib /pdb:bin\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.pdb /version:0.0 /machine:x64 /debug /INCREMENTAL /subsystem:console  && cmd.exe /C "cd /D %SRC_DIR%\build && %BUILD_PREFIX%\Library\bin\cmake.exe -E create_symlink warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe D:/bld/warpx_1614915530525/work/build/bin/warpx""
    FAILED: bin/warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe
    cmd.exe /C "cd . && %BUILD_PREFIX%\Library\bin\cmake.exe -E vs_link_exe --intdir=CMakeFiles\app.dir --rc=C:\PROGRA~2\WI3CF2~1\10\bin\100177~1.0\x64\rc.exe --mt=C:\PROGRA~2\WI3CF2~1\10\bin\100177~1.0\x64\mt.exe --manifests  -- %BUILD_PREFIX%\Library\bin\lld-link.exe  @CMakeFiles\app.rsp  /out:bin\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe /implib:lib\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.lib /pdb:bin\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.pdb /version:0.0 /machine:x64 /debug /INCREMENTAL /subsystem:console  && cmd.exe /C "cd /D %SRC_DIR%\build && %BUILD_PREFIX%\Library\bin\cmake.exe -E create_symlink warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe D:/bld/warpx_1614915530525/work/build/bin/warpx""
    LINK Pass 1: command "%BUILD_PREFIX%\Library\bin\lld-link.exe @CMakeFiles\app.rsp /out:bin\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe /implib:lib\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.lib /pdb:bin\warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.pdb /version:0.0 /machine:x64 /debug /INCREMENTAL /subsystem:console /MANIFEST /MANIFESTFILE:CMakeFiles\app.dir/intermediate.manifest CMakeFiles\app.dir/manifest.res" failed (exit code 1) with the following output:
    lld-link: error: could not open 'm.lib': no such file or directory
    ```

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@
 if not exist %LIBRARY_PREFIX%\bin md %LIBRARY_PREFIX%\bin
 if errorlevel 1 exit 1
 
-for %%d in (2 3) do (
+for %%d in (2 3 RZ) do (
     cmake ^
         -S . -B build                         ^
         -G "Ninja"                            ^
@@ -20,7 +20,7 @@ for %%d in (2 3) do (
         -DWarpX_MPI=OFF     ^
         -DWarpX_OPENPMD=ON  ^
         -DWarpX_openpmd_internal=OFF ^
-        -DWarpX_PSATD=ON    ^
+        -DWarpX_PSATD=OFF   ^
         -DWarpX_QED=ON      ^
         -DWarpX_DIMS=%%d    ^
         %SRC_DIR%
@@ -47,44 +47,6 @@ for %%d in (2 3) do (
 :: future (if skipping AMReX headers) - inside above loop
 ::  cmake --build build --config RelWithDebInfo --target install
 ::  if errorlevel 1 exit 1
-
-:: RZ (until BLAS++ and LAPACK++ are shipped with conda-forge)
-cmake ^
-    -S . -B build                         ^
-    -G "Ninja"                            ^
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo     ^
-    -DCMAKE_C_COMPILER=clang-cl           ^
-    -DCMAKE_CXX_COMPILER=clang-cl         ^
-    -DCMAKE_LINKER=lld-link               ^
-    -DCMAKE_VERBOSE_MAKEFILE=ON           ^
-    -DWarpX_ASCENT=OFF  ^
-    -DWarpX_LIB=ON      ^
-    -DWarpX_MPI=OFF     ^
-    -DWarpX_OPENPMD=ON  ^
-    -DWarpX_openpmd_internal=OFF ^
-    -DWarpX_PSATD=OFF   ^
-    -DWarpX_QED=ON      ^
-    -DWarpX_DIMS=%%d    ^
-    %SRC_DIR%
-if errorlevel 1 exit 1
-
-cmake --build build --config RelWithDebInfo --parallel 2
-if errorlevel 1 exit 1
-
-for /r "build\bin" %%f in (*.exe) do (
-    echo %%~nf
-    dir
-    copy build\bin\%%~nf.exe %LIBRARY_PREFIX%\bin\
-    if errorlevel 1 exit 1
-)
-for /r "build\lib" %%f in (*.dll) do (
-    echo %%~nf
-    dir
-    copy build\lib\%%~nf.dll %LIBRARY_PREFIX%\lib\
-    if errorlevel 1 exit 1
-)
-
-rmdir /s /q build
 
 :: add Python API (PICMI interface)
 set "PYWARPX_LIB_DIR=%LIBRARY_PREFIX%\lib"
