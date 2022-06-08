3480f39c66cf105bc48d2eb01bdd583bdee16679
Axel Huebl <axel.huebl@plasma.ninja>
2022-06-08 02:22:53 -0700

    Update: Executable Names (add `.PDP`)

--- recipe/test.bat
+++ recipe/test.bat
@@ -4,19 +4,19 @@ set "OMP_NUM_THREADS=2"
 set "TEST_DIR=Examples\Physics_applications\laser_acceleration"
 
 :: 3D
-warpx.3d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe %TEST_DIR%\inputs_3d max_step=50 diag1.intervals=10 diag1.format=openpmd
+warpx.3d.NOMPI.OMP.DP.PDP.OPMD.PSATD.QED.exe %TEST_DIR%\inputs_3d max_step=50 diag1.intervals=10 diag1.format=openpmd
 if errorlevel 1 exit 1
 
 :: 2D
-warpx.2d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe %TEST_DIR%\inputs_2d max_step=50 diag1.intervals=10 diag1.format=openpmd
+warpx.2d.NOMPI.OMP.DP.PDP.OPMD.PSATD.QED.exe %TEST_DIR%\inputs_2d max_step=50 diag1.intervals=10 diag1.format=openpmd
 if errorlevel 1 exit 1
 
 :: 1D
-warpx.1d.NOMPI.OMP.DP.OPMD.PSATD.QED.exe %TEST_DIR%\inputs_1d max_step=50 diag1.intervals=10 diag1.format=openpmd
+warpx.1d.NOMPI.OMP.DP.PDP.OPMD.PSATD.QED.exe %TEST_DIR%\inputs_1d max_step=50 diag1.intervals=10 diag1.format=openpmd
 if errorlevel 1 exit 1
 
 :: RZ
-warpx.RZ.NOMPI.OMP.DP.OPMD.PSATD.QED.exe %TEST_DIR%\inputs_rz max_step=50 diag1.intervals=10 diag1.format=openpmd diag1.fields_to_plot=Er Et Ez Br Bt Bz jr jt jz rho
+warpx.RZ.NOMPI.OMP.DP.PDP.OPMD.PSATD.QED.exe %TEST_DIR%\inputs_rz max_step=50 diag1.intervals=10 diag1.format=openpmd diag1.fields_to_plot=Er Et Ez Br Bt Bz jr jt jz rho
 if errorlevel 1 exit 1
 
 :: Python: 3D
