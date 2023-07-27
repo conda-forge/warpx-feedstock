878ca910db19d8cf0ae1e49e604bda22a6f9d84c
Axel Huebl <axel.huebl@plasma.ninja>
2023-07-27 00:27:51 -0700

    Update Deps and Multi-Dim Build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,45 +5,43 @@
 if not exist %LIBRARY_PREFIX%\bin md %LIBRARY_PREFIX%\bin
 if errorlevel 1 exit 1
 
-for %%d in (1 2 3 RZ) do (
-    cmake ^
-        -S %SRC_DIR% -B build                 ^
-        %CMAKE_ARGS%                          ^
-        -G "Ninja"                            ^
-        -DCMAKE_BUILD_TYPE=Release            ^
-        -DCMAKE_C_COMPILER=clang-cl           ^
-        -DCMAKE_CXX_COMPILER=clang-cl         ^
-        -DCMAKE_LINKER=lld-link               ^
-        -DCMAKE_NM=llvm-nm                    ^
-        -DCMAKE_VERBOSE_MAKEFILE=ON           ^
-        -DWarpX_ASCENT=OFF  ^
-        -DWarpX_LIB=ON      ^
-        -DWarpX_MPI=OFF     ^
-        -DWarpX_OPENPMD=ON  ^
-        -DWarpX_openpmd_internal=OFF ^
-        -DWarpX_PSATD=ON    ^
-        -DWarpX_QED=ON      ^
-        -DWarpX_DIMS=%%d
-    if errorlevel 1 exit 1
+cmake ^
+    -S %SRC_DIR% -B build                 ^
+    %CMAKE_ARGS%                          ^
+    -G "Ninja"                            ^
+    -DCMAKE_BUILD_TYPE=Release            ^
+    -DCMAKE_C_COMPILER=clang-cl           ^
+    -DCMAKE_CXX_COMPILER=clang-cl         ^
+    -DCMAKE_LINKER=lld-link               ^
+    -DCMAKE_NM=llvm-nm                    ^
+    -DCMAKE_VERBOSE_MAKEFILE=ON           ^
+    -DWarpX_ASCENT=OFF  ^
+    -DWarpX_LIB=ON      ^
+    -DWarpX_MPI=OFF     ^
+    -DWarpX_OPENPMD=ON  ^
+    -DWarpX_openpmd_internal=OFF ^
+    -DWarpX_PSATD=ON    ^
+    -DWarpX_QED=ON      ^
+    -DWarpX_DIMS="1;2;RZ;3"
+if errorlevel 1 exit 1
 
-    cmake --build build --config Release --parallel 2
-    if errorlevel 1 exit 1
+cmake --build build --config Release --parallel 2
+if errorlevel 1 exit 1
 
-    for /r "build\bin" %%f in (*.exe) do (
-        echo %%~nf
-        dir
-        copy build\bin\%%~nf.exe %LIBRARY_PREFIX%\bin\
-        if errorlevel 1 exit 1
-    )
-    for /r "build\lib" %%f in (*.dll) do (
-        echo %%~nf
-        dir
-        copy build\lib\%%~nf.dll %LIBRARY_PREFIX%\lib\
-        if errorlevel 1 exit 1
-    )
-    
-    rmdir /s /q build
+for /r "build\bin" %%f in (*.exe) do (
+    echo %%~nf
+    dir
+    copy build\bin\%%~nf.exe %LIBRARY_PREFIX%\bin\
+    if errorlevel 1 exit 1
 )
+for /r "build\lib" %%f in (*.dll) do (
+    echo %%~nf
+    dir
+    copy build\lib\%%~nf.dll %LIBRARY_PREFIX%\lib\
+    if errorlevel 1 exit 1
+)
+
+rmdir /s /q build
 :: future (if skipping AMReX headers) - inside above loop
 ::  cmake --build build --config Release --target install
 ::  if errorlevel 1 exit 1
